a51de1599779baccd25903a34be9b5910fe9c7b3
Marc Vlad <marc@deltav.ai>
2026-07-02 14:45:22 +0300

    IntelHub: standalone dashboard with pipeline + Artemis + DeFiLlama

---
+++ _next/static/QxZ456yANZeksr_e5ljWU/_buildManifest.js
@@ -0,0 +1,11 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/website-private/_app",
+    "/website-private/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
